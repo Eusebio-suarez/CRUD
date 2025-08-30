db8b03a1368da5656088db67300fbfefe4f2ba63
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-08-30 14:09:39 -0500

    configuracion inicial

---
+++ src/test/java/com/springBoot/CRUD/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
